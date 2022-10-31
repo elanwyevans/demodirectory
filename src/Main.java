a6861948af0d5333cfdbf5e61d0b2dbd8ba6fcc0
Elanwy <elanwy.evans@live.co.uk>
2022-10-31 22:30:58 +0000

    Committing change to cause merge conflict

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args)
-    System.out.println("Hello world!");
+    System.out.println("This is a change that will cause a merge conflict");
     System.out.println("Hello again!");
     System.out.println("Hello it's a me, Mario!");
     System.out.println("This is an entry made on branch1");
