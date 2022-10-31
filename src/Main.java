4679523c050131c6f9a3a47f6573636913fa31c9
Elanwy <elanwy.evans@live.co.uk>
2022-10-31 22:10:51 +0000

    Commit of branch1

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     System.out.println("Hello world!");
     System.out.println("Hello again!");
     System.out.println("Hello it's a me, Mario!");
+    System.out.println("This is an entry made on branch1");
 
     }
 }
