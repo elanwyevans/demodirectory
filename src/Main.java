7bd49638424b02d97b6f6136025c793f40c56c1a
Elanwy <elanwy.evans@live.co.uk>
2022-10-31 22:00:07 +0000

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    System.out.println("Hello world!");
+    System.out.println("Hello again!");
+    System.out.println("Hello it's a me, Mario!");
+
     }
 }
